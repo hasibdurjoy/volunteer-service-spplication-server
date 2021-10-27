1d8d97b5cfad391ab3e7a7b7eb22336e0b711900
hasibdurjoy <hrd2571@gmail.com>
2021-10-27 23:32:02 +0600

    update

--- index.js
+++ index.js
@@ -42,12 +42,12 @@ async function run() {
             res.send(service);
         })
 
-        app.get('/services/:name', async (req, res) => {
-            const name = req.params.name;
-            const query = { name: name };
+        /* app.get('/services/:key', async (req, res) => {
+            const key = req.params.key;
+            const query = { key: key };
             const service = await serviceCollection.findOne(query);
             res.send(service);
-        })
+        }) */
 
         app.get('/volunteers/:email', async (req, res) => {
             const email = req.params.email;
@@ -62,7 +62,15 @@ async function run() {
             const order = req.body;
             const result = await volunteerCollection.insertOne(order)
             res.json(result);
-        })
+        });
+
+        // DELETE API
+        app.delete('/volunteers/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await volunteerCollection.deleteOne(query);
+            res.json(result);
+        });
     }
     finally {
         // await client.close();
